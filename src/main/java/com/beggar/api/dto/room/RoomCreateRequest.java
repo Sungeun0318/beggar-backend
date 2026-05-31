ac6bf59e02519f77f0751c650559f3d0b548ea63
so0hub <so0mile02@gmail.com>
2026-06-01 01:00:59 +0900

    fix : RoomCreateRequest , RoomResponse 수정

---
+++ src/main/java/com/beggar/api/dto/room/RoomCreateRequest.java
@@ -0,0 +1,33 @@
+package com.beggar.api.dto.room;
+
+import java.util.List;
+
+public class RoomCreateRequest {
+    private String roomName;
+    private List<String> tags;
+    private Boolean isFriends;
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Boolean getFriends() {
+        return isFriends;
+    }
+
+    public void setFriends(Boolean friends) {
+        isFriends = friends;
+    }
+}
--- src/main/java/com/beggar/api/dto/room/RoomResponse.java
+++ src/main/java/com/beggar/api/dto/room/RoomResponse.java
@@ -1,32 +1,17 @@
 package com.beggar.api.dto.room;
 
-import com.beggar.api.entity.Room;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record RoomResponse(
-        Long roomNo,
-        String roomName,
-        String roomCode,
-        Long ownerUserNo,
-        Integer maxMemberCount,
-        Integer totalBudget,
-        Boolean isFriends,
-        LocalDateTime roomCreated,
-        List<String> tags
-) {
-    public static RoomResponse of(Room room, List<String> tags) {
-        return new RoomResponse(
-                room.getRoomNo(),
-                room.getRoomName(),
-                room.getRoomCode(),
-                room.getOwner().getUserNo(),
-                room.getMaxMemberCount(),
-                room.getTotalBudget(),
-                room.getIsFriends(),
-                room.getRoomCreated(),
-                tags
-        );
-    }
+public class RoomResponse {
+    private Long roomNo;
+    private String roomName;
+    private String roomCode;
+    private Long ownerUserNo;
+    private Integer totalBudget;
+    private boolean isFriends;
+    private LocalDateTime roomCreated;
+    private List<String> tags;
+
+
 }
