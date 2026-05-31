c061e4817394979b654d4dffeb64ab12acea07d0
so0hub <so0mile02@gmail.com>
2026-06-01 01:30:45 +0900

    fix : RoomResponse 코드 수정

--- src/main/java/com/beggar/api/dto/room/RoomResponse.java
+++ src/main/java/com/beggar/api/dto/room/RoomResponse.java
@@ -1,17 +1,17 @@
 package com.beggar.api.dto.room;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import java.time.LocalDateTime;
-import java.util.List;
 
+@Getter
+@AllArgsConstructor // 💡 모든 필드를 파라미터로 받는 생성자를 롬복이 자동으로 만들어줍니다!
 public class RoomResponse {
     private Long roomNo;
     private String roomName;
     private String roomCode;
     private Long ownerUserNo;
     private Integer totalBudget;
-    private boolean isFriends;
+    private Boolean isFriends;
     private LocalDateTime roomCreated;
-    private List<String> tags;
-
-
 }
