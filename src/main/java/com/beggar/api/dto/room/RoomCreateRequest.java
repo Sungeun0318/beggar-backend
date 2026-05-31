c19861f1c914ef3ea1ca3091923f6d16d0fadf6c
so0hub <so0mile02@gmail.com>
2026-06-01 01:30:05 +0900

    fix : RoomCreateRequest 코드 수정

--- src/main/java/com/beggar/api/dto/room/RoomCreateRequest.java
+++ src/main/java/com/beggar/api/dto/room/RoomCreateRequest.java
@@ -1,33 +1,13 @@
 package com.beggar.api.dto.room;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
+@Getter
+@NoArgsConstructor
 public class RoomCreateRequest {
     private String roomName;
     private List<String> tags;
     private Boolean isFriends;
-
-    public String getRoomName() {
-        return roomName;
-    }
-
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
-    public Boolean getIsFriends() {
-        return isFriends;
-    }
-
-    public void setFriends(Boolean friends) {
-        isFriends = friends;
-    }
 }
