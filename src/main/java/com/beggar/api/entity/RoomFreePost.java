1fd5d68274ce0ee1b0e1f8fc63a751560b93bd32
leehyeon2021 <leehyeon2021@gmail.com>
2026-06-01 13:02:28 +0900

    Feat: RoomFree 관련 엔티티 및 리포지토리 레이어 추가 (RoomFreePost, RoomFreeComment, RoomFreeChat)

---
+++ src/main/java/com/beggar/api/entity/RoomFreePost.java
@@ -0,0 +1,46 @@
+package com.beggar.api.entity;
+
+import com.beggar.api.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "room_free_posts", indexes = {
+        @Index(name = "idx_rf_post_created", columnList = "created_at DESC")
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoomFreePost extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_no", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_rf_posts_user"))
+    private User author;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RoomFreeComment> comments = new ArrayList<>();
+
+    @Builder
+    public RoomFreePost(User author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+}
