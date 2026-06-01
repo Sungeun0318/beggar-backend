dac2fd155ba198af8bd2cdbe92476215d88c9192
dokyung253-lang <dokyung253@gmail.com>
2026-06-01 16:32:34 +0900

    FEAT : 회원가입 POST 추가

--- src/main/java/com/beggar/api/controller/UserController.java
+++ src/main/java/com/beggar/api/controller/UserController.java
@@ -1,30 +1,42 @@
-package com.beggar.api.controller;
-
-import com.beggar.api.dto.user.UserRequest;
-import com.beggar.api.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/users")
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserService userService;
-    // ToDO : Post /users/signup            - 회원가입
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody UserRequest userRequest){
-        System.out.println("userRequest = " + userRequest);
-
-        userService.userSignup(userRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
-    }
-    // TODO: GET    /users/me               — 내 프로필 조회
-    // TODO: GET    /users/me/beggar-score  — 내 거지력 점수/칭호
-    // TODO: DELETE /users/me               — 회원 탈퇴
-}
+//package com.beggar.api.controller;
+//
+//import com.beggar.api.dto.user.UserRequest;
+//import com.beggar.api.security.JwtTokenProvider;
+//import com.beggar.api.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/users")
+//@RequiredArgsConstructor
+//public class UserController {
+//
+//    private final UserService userService;
+//    // ToDO : Post /users/signup            - 회원가입
+//    @PostMapping("/signup")
+//    public ResponseEntity<?> signup(@RequestBody UserRequest userRequest){
+//        userService.userSignup(userRequest);
+//        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
+//    }
+//    // 로그인
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody UserRequest userRequest){
+//        boolean result = UserService.login(userRequest);
+//        if{
+//            String token = JwtTokenProvider.createToken(userRequest.getEmail());
+//            System.out.println("userRequest = " + userRequest);
+//            return ResponseEntity.ok("로그인 성공");
+//                    .header("Authorization", "Bearer" + token)
+//                    .body(true);
+//        }
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("아이디 또는 비밀번호가 틀렸습니다");
+//    }
+//     TODO: GET    /users/me               — 내 프로필 조회
+//     TODO: GET    /users/me/beggar-score  — 내 거지력 점수/칭호
+//     TODO: DELETE /users/me               — 회원 탈퇴
+//}
--- src/main/java/com/beggar/api/security/JwtTokenProvider.java
+++ src/main/java/com/beggar/api/security/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.beggar.api.security;
 
+import lombok.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,8 @@ public class JwtTokenProvider {
     // TODO: createRefreshToken(userNo)  — 동일하게 리프레시 토큰 발급
     // TODO: parseUserNo(token)          — 토큰 → userNo 추출
     // TODO: isValid(token)              — 서명 검증 + 만료 체크
-    //
+
+
     // application.properties:
     //   jwt.secret, jwt.access-token-validity-ms, jwt.refresh-token-validity-ms
 }
