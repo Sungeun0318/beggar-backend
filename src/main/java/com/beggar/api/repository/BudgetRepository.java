0df6871e5ea400b5e43d6e95064aa6427eabe601
so0hub <so0mile02@gmail.com>
2026-06-05 09:41:43 +0900

    fix : 예산 로직

--- src/main/java/com/beggar/api/repository/BudgetRepository.java
+++ src/main/java/com/beggar/api/repository/BudgetRepository.java
@@ -2,33 +2,14 @@ package com.beggar.api.repository;
 
 import com.beggar.api.entity.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
 
-    Optional<Budget> findByRoomMember_RoomMemberId(Long roomMemberId);
+    Optional<Budget> findByRoomNoAndUserNo(Long roomNo, Long userNo);
 
-    @Query("""
-           select b
-             from Budget b
-            where b.roomMember.room.roomNo = :roomNo
-           """)
-    List<Budget> findAllByRoomNo(Long roomNo);
+    List<Budget> findByRoomNo(Long roomNo);
 
-    @Query("""
-           select count(b)
-             from Budget b
-            where b.roomMember.room.roomNo = :roomNo
-           """)
     long countByRoomNo(Long roomNo);
-
-    @Query("""
-           select coalesce(min(b.budgetAmount), 0)
-             from Budget b
-            where b.roomMember.room.roomNo = :roomNo
-           """)
-    int findMinBudgetByRoomNo(Long roomNo);
 }
