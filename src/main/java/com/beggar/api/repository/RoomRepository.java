bd0427d115fcc6a23a5f596d64d333ba85a96d75
so0hub <so0mile02@gmail.com>
2026-06-06 23:27:35 +0900

    Fix: RoomRepository 문법 오류 수정 및 필수 메서드 추가

--- src/main/java/com/beggar/api/repository/RoomRepository.java
+++ src/main/java/com/beggar/api/repository/RoomRepository.java
@@ -2,11 +2,19 @@ package com.beggar.api.repository;
 
 import com.beggar.api.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room,Long>{
+public interface RoomRepository extends JpaRepository<Room, Long> {
     Optional<Room> findByRoomCode(String roomCode);
+
+    boolean existsByRoomCode(String roomCode);
+
+    @Query("SELECT rm.room FROM RoomMember rm " +
+           "WHERE rm.user.userNo = :userNo AND rm.status = com.beggar.api.entity.RoomMember.Status.ACTIVE " +
+           "ORDER BY rm.room.roomCreated DESC")
+    java.util.List<Room> findActiveRoomsByUserNo(Long userNo);
 }
