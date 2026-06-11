62ee4ee5b0edfde33e41b6dcee45b2a9eeb03829
leehyeon2021 <leehyeon2021@gmail.com>
2026-06-11 22:09:44 +0900

    Chore: 단일 서버 환경 크래시 방지를 위해 Redis AutoConfiguration 제외

--- src/main/java/com/beggar/api/BeggarApplication.java
+++ src/main/java/com/beggar/api/BeggarApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        // 서버 확장 시 Redis 전환이 가능하도록 의존성은 유지하되, 구동 크래시 방지를 위해 단일 환경에서의 자동 연결 시도 제외
+        org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class
+})
 public class BeggarApplication {
 
     public static void main(String[] args){
